a0f12abec19931c5fad5895b1a4f414154ff410d
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-10-29 11:11:01 -0200

    test: Add initial specs to I18n class

--- spec/i18n_spec.cr
+++ spec/i18n_spec.cr
@@ -1,9 +1,25 @@
 require "./spec_helper"
 
 describe I18n do
-  # TODO: Write tests
+  I18n.load_path = File.join({__DIR__, "./fixtures"})
 
-  it "works" do
-    false.should eq(true)
+  it "should load english simple case" do
+    I18n.locale = :en
+    I18n.translate("hello").should eq("hello")
+  end
+
+  it "should load english nested case" do
+    I18n.locale = :en
+    I18n.translate("home.message").should eq("hello, world!")
+  end
+
+  it "should load portuguese simple case" do
+    I18n.locale = :"pt-BR"
+    I18n.translate("hello").should eq("oi")
+  end
+
+  it "should load portuguese nested case" do
+    I18n.locale = :"pt-BR"
+    I18n.translate("home.message").should eq("olá, mundo!")
   end
 end
